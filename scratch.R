4641f7a20070dbd02bc88123de82145053ad9224
Bradley Saul <bradleysaul@gmail.com>
2015-06-25 14:54:05 -0400

    -starts a vignette

--- scratch.R
+++ scratch.R
@@ -1,23 +1,4 @@
 
-#### TESTING GROUNDS ####
-
-myMap <- aes_map(x = 'start', 
-                 xend = 'stop',
-                 color = 'drug_1',
-                 shape  = 'outcome',
-                 vline1 = 'withdrew',
-                 vline2 = 'current_status',
-                 vline3 = 'major_event')
-
-
-myBaseplot  <- make_baseplot(patientsamples,
-                         aes_map = myMap,
-                         axis.lim.x    = c(-7,(78*7)+15),
-                         axis.breaks.x = c(0, 4*7, 12*7, 29*7, 52*7, 78*7),
-                         axis.labels.x = c(0, 4, 12, 29, 52, 78),
-                         axis.breaks.y = c(.2, .5, .75, .85),
-                         axis.labels.y = c('R', 'P', 'ORCS', 'IVCS')) +
-                theme(axis.ticks.y = element_blank())
 
 ## Create all plots by subject ##
 allPlots <- dlply(dt, 'subjectid', function(x){
